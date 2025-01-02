395fb052877856ff888106d8111d0ca47252440f
msk-saad <developersaad12@gmail.com>
2025-01-02 18:27:38 +0530

    adding styles

--- vite-react-calculator/src/App.jsx
+++ vite-react-calculator/src/App.jsx
@@ -1,57 +1,62 @@
 import React, { useState } from 'react'
+import './styles/app.css'
 
 const App = () => {
 
   const [calculation, setCalculation] = useState('');
 
-  const displayFunction = () => {
+  const displayFunction = (event) => {
     const clickedVal = event.target.value;
     setCalculation(preVal => preVal + clickedVal);
   }
 
   return (
     <>
-      <div className="calc">
+      <div className='container'>
+        <div className="calc">
 
-        <div className="display">
-          <input type="text" value = {calculation}/>
+          <form action="">
+
+          <div className="display">
+            <input type="text" defaultValue={calculation} />
+          </div>
+
+            <p>
+              <input type="button" defaultValue="1" onClick={displayFunction} />
+              <input type="button" defaultValue="2" onClick={displayFunction} />
+              <input type="button" defaultValue="3" onClick={displayFunction} />
+              <input type="button" defaultValue="+" onClick={displayFunction} />
+            </p>
+
+
+            <p>
+              <input type="button" defaultValue="4" onClick={displayFunction} />
+              <input type="button" defaultValue="5" onClick={displayFunction} />
+              <input type="button" defaultValue="6" onClick={displayFunction} />
+              <input type="button" defaultValue="-" onClick={displayFunction} />
+            </p>
+
+            <p>
+              <input type="button" defaultValue="7" onClick={displayFunction} />
+              <input type="button" defaultValue="8" onClick={displayFunction} />
+              <input type="button" defaultValue="9" onClick={displayFunction} />
+              <input type="button" defaultValue="*" onClick={displayFunction} />
+            </p>
+
+            <p>
+              <input type="button" defaultValue="0" onClick={displayFunction} />
+              <input type="button" defaultValue="." onClick={displayFunction} />
+              <input type="button" defaultValue="=" onClick={e => setCalculation(eval(calculation))} />
+              <input type="button" defaultValue="/" onClick={displayFunction} />
+            </p>
+
+            <p>
+              <input type="button" className='last-btns' defaultValue="AC" onClick={e => setCalculation('')} />
+              <input type="button" defaultValue="Del" className='last-btns' onClick={e => setCalculation(calculation.slice(0, -1))} />
+            </p>
+          </form>
         </div>
-
-        <p>
-          <input type="button" value="1" onClick={displayFunction}/>
-          <input type="button" value="2" onClick={displayFunction}/>
-          <input type="button" value="3" onClick={displayFunction}/>
-          <input type="button" value="+" onClick={displayFunction}/>
-        </p>
-
-
-        <p>
-          <input type="button" value="4" onClick={displayFunction}/>
-          <input type="button" value="5" onClick={displayFunction}/>
-          <input type="button" value="6" onClick={displayFunction}/>
-          <input type="button" value="-" onClick={displayFunction}/>
-        </p>
-
-        <p>
-          <input type="button" value="7" onClick={displayFunction}/>
-          <input type="button" value="8" onClick={displayFunction}/>
-          <input type="button" value="9" onClick={displayFunction}/>
-          <input type="button" value="*" onClick={displayFunction}/>
-        </p>
-
-        <p>
-          <input type="button" value="0" onClick={displayFunction}/>
-          <input type="button" value="." onClick={displayFunction}/>
-          <input type="button" value="=" onClick={e => setCalculation(eval(calculation))}/>
-          <input type="button" value="/" onClick={displayFunction}/>
-        </p>
-
-        <p>
-          <input type="button" value="AC" onClick={e => setCalculation('')}/>
-          <input type="button" value="Del" onClick={e => setCalculation(calculation.slice(0,-1))}/>
-        </p>
       </div>
-
     </>
   )
 }
